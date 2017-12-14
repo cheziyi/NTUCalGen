e5882e4ac28001f5e649408f8742d67d505c8982
Calvin Che <calvin@cczy.io>
2017-12-14 19:28:36 +0800

    Initial commit.

---
+++ NTUCalGen/Default.aspx
@@ -0,0 +1,173 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="NTUCalGen.Default" ValidateRequest="false" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+
+    <title>NTUCalGen - Generate an iCal calendar from NTU's course timetable</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main.css" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="assets/js/hover.zoom.js"></script>
+    <script src="assets/js/hover.zoom.conf.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+    <!-- Static navbar -->
+    <div class="navbar navbar-inverse navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="Default.aspx">NTUCalGen</a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <%--                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="Default.aspx">Generate iCal</a></li>
+                </ul>--%>
+            </div>
+            <!--/.nav-collapse -->
+        </div>
+    </div>
+
+
+    <div class="container pt">
+        <div class="row mt">
+            <div class="col-lg-12">
+
+                <div class="row mt">
+                    <div class="col-lg-12 col-lg-offset-0 centered">
+                        <h3>Generate an iCal calendar from NTU's course timetable.</h3>
+                        <hr>
+
+                        <p>
+                            View your NTU Timetable on your smartphone / computer!<br />
+                            Works with Android, iPhone, Microsoft Outlook, Google Calendar, etc.<br />
+                            Even includes a map of your class location!<br />
+                            <br />
+                            <img src="images/screenshot1.png" width="200" />
+                            <img src="images/screenshot2.png" width="200" />
+                        </p>
+                    </div>
+                </div>
+                <div class="row mt">
+                    <form id="form1" runat="server">
+                        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+                        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+                            <ContentTemplate>
+                                Step 1.1: Log into <a href="https://intu.ntu.edu.sg/_layouts/iNTU/Main.aspx?Page=StudentLink">NTU StudentLink</a><br />
+                                Step 1.2: Click on "Degree Audit"<br />
+                                Step 1.3: Click on "View Course timetable"<br />
+                                Step 1.4: Right-click on the timetable and select "View Source"<br />
+                                Step 1.5: Select and paste everything into the textbox below<br />
+                                <br />
+                                <h4>Step 2: If you see the HTML source below, click on "Parse!"</h4>
+                                <div class="form-group">
+
+                                    <asp:TextBox ID="txtSrc" runat="server" CssClass="form-control" TextMode="MultiLine" placeholder="HTML Script" Rows="5"></asp:TextBox>
+
+                                </div>
+                                <div class="form-group">
+                                    <div class="checkbox">
+                                        <label>
+                                            <asp:CheckBox ID="chkAbbr" runat="server" />
+                                            Abbreviate Subject Titles</label>
+                                    </div>
+                                </div>
+
+                                <asp:HiddenField ID="hidMatric" runat="server" />
+                            </ContentTemplate>
+                        </asp:UpdatePanel>
+                        <asp:Button ID="btnParse" runat="server" Text="Parse!" CssClass="btn btn-success" OnClick="btnParse_Click" />
+
+                        <p>
+                            <br />
+                            <asp:Label ID="lblError" runat="server" Text="iCal will be downloaded once you click on 'Parse!'"></asp:Label>
+                            <br />
+                            To import calendar to: <a href="https://play.google.com/store/apps/details?id=tk.drlue.icalimportexport&hl=en">Android</a> | <a href="https://support.google.com/calendar/answer/37118?hl=en">Google</a> | <a href="https://www.google.com/search?q=iPhone+import+iCal">iPhone</a> | <a href="https://support.office.com/en-us/article/Import-iCal-or-Address-Book-items-into-Outlook-2a637ac6-f3b5-411d-8a73-016bd90c1094">Outlook</a>
+
+                        </p>
+
+                        <asp:UpdateProgress ID="UpdateProgress1" runat="server">
+                            <ProgressTemplate>
+                                <div class="modal" style="display: block; background-color: #222222; opacity: 0.4;" aria-hidden="false">
+                                </div>
+                                <div class="modal" style="display: block;" aria-hidden="false">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-body">
+                                                Loading...
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </ProgressTemplate>
+                        </asp:UpdateProgress>
+                    </form>
+
+                </div>
+            </div>
+            <!-- /row -->
+        </div>
+    </div>
+    <!-- /container -->
+
+    <div id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+
+                    <p>Developed by <a href="http://cczy.io">Calvin Che</a>. For any feedback/bugs, please email to <a href="mailto:calvin@cczy.io">calvin@cczy.io</a>. Thanks!</p>
+                </div>
+                <!-- /col-lg-4 -->
+
+            </div>
+
+        </div>
+    </div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/bootstrap.min.js"></script>
+
+
+    <script>
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date(); a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-64593206-2', 'auto');
+        ga('send', 'pageview');
+
+    </script>
+
+</body>
+</html>
---
+++ NTUCalGen/Default.aspx.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Configuration;
+using System.Web.UI.WebControls;
+
+namespace NTUCalGen
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        //DateTime start;
+        string icalOut = "";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnParse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                String title = WebConfigurationManager.AppSettings["fileTitle"];
+
+                HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                doc.LoadHtml(txtSrc.Text);
+
+                List<List<string>> table = doc.DocumentNode.SelectSingleNode("//table")
+                .Descendants("tr")
+                .Skip(1)
+                .Where(tr => tr.Elements("td").Count() > 1)
+                .Select(tr => tr.Elements("td").Select(td => td.InnerText.Trim()).ToList())
+                .ToList();
+
+                HtmlAgilityPack.HtmlNode bnode = doc.DocumentNode.SelectSingleNode("//b");
+                title += bnode.InnerText;
+
+                title += hidMatric.Value;
+
+                for (int i = 0; i < table.Count; i++)
+                {
+                    if (CheckBlank(table, i, 0) && i != 0)
+                    {
+                        for (int j = 0; j < table[i].Count; j++)
+                        {
+                            if (CheckBlank(table, i, j))
+                            {
+                                table[i][j] = table[i - 1][j];
+                            }
+                        }
+                    }
+                }
+
+                icalOut = "BEGIN:VCALENDAR" + "\r\n";
+                icalOut += "PRODID: -//CCZY//ntucalgen.cczy.io//" + "\r\n";
+                icalOut += "VERSION:2.0" + "\r\n";
+                icalOut += "BEGIN:VTIMEZONE" + "\r\n";
+                icalOut += "TZID:Singapore Standard Time" + "\r\n";
+                icalOut += "BEGIN:STANDARD" + "\r\n";
+                icalOut += "DTSTART:16010101T000000" + "\r\n";
+                icalOut += "TZOFFSETFROM:+0800" + "\r\n";
+                icalOut += "TZOFFSETTO:+0800" + "\r\n";
+                icalOut += "END:STANDARD" + "\r\n";
+                icalOut += "END:VTIMEZONE" + "\r\n";
+
+                for (int i = 0; i < table.Count; i++)
+                {
+                    if (!CheckBlank(table, i, 11))
+                    {
+                        String dateStartStr = WebConfigurationManager.AppSettings["semStart"];
+
+                        DateTime tempStart = DateTime.Parse(dateStartStr);
+                        DateTime tempEnd = DateTime.Parse(dateStartStr);
+
+                        int dayofweek = 0;
+                        switch (table[i][11])
+                        {
+                            case "MON":
+                                dayofweek = 0;
+                                break;
+                            case "TUE":
+                                dayofweek = 1;
+                                break;
+                            case "WED":
+                                dayofweek = 2;
+                                break;
+                            case "THU":
+                                dayofweek = 3;
+                                break;
+                            case "FRI":
+                                dayofweek = 4;
+                                break;
+                            default:
+                                break;
+                        }
+
+                        tempStart = tempStart.AddDays(dayofweek);
+                        tempEnd = tempEnd.AddDays(dayofweek);
+
+                        if (chkAbbr.Checked)
+                        {
+                            table[i][1] = Abbr(table[i][1]);
+                        }
+
+
+                        if (table[i][14].StartsWith("Teaching Wk"))
+                        {
+                            table[i][14] = table[i][14].Substring(11);
+
+                            string[] times = table[i][12].Split('-');
+
+                            int start = Convert.ToInt32(times[0]);
+                            tempStart = tempStart.AddHours(start / 100);
+                            tempStart = tempStart.AddMinutes(start % 100);
+
+                            int end = Convert.ToInt32(times[1]);
+                            tempEnd = tempEnd.AddHours(end / 100);
+                            tempEnd = tempEnd.AddMinutes(end % 100);
+
+
+
+
+                            string[] days = table[i][14].Split(',');
+                            foreach (string day in days)
+                            {
+
+                                if (day.Contains("-"))
+                                {
+                                    string[] days2 = day.Split('-');
+
+                                    for (int k = Convert.ToInt32(days2[0]) - 1; k < Convert.ToInt32(days2[1]); k++)
+                                    {
+                                        int week = k;
+
+                                        if (week > 6)
+                                            week += 1;
+
+                                        icalOut += GenerateICalEvent(tempStart.AddDays(week * 7), tempEnd.AddDays(week * 7), table[i]);
+                                    }
+                                }
+                                else
+                                {
+                                    int week = Convert.ToInt32(day) - 1;
+                                    if (week > 6)
+                                        week += 1;
+
+                                    icalOut += GenerateICalEvent(tempStart.AddDays(week * 7), tempEnd.AddDays(week * 7), table[i]);
+                                }
+                            }
+
+                        }
+                    }
+                }
+
+                icalOut += "END:VCALENDAR";
+
+                Response.Clear();
+                Response.ClearHeaders();
+
+                Response.AddHeader("Content-Length", icalOut.Length.ToString());
+                Response.ContentType = "text/plain";
+                Response.AppendHeader("content-disposition", "attachment;filename=\"" + title + ".ics\"");
+
+                Response.Write(icalOut);
+
+                Response.End();
+
+                //lblError.ForeColor = System.Drawing.Color.Green;
+                //lblError.Text = "Your iCal is generated!";
+
+                //Response.AddHeader("Content-disposition", "attachment; filename=" + title+ ".ics");
+                //Response.ContentType = "text/plain";
+                //Response.BinaryWrite(GetBytes(icalOut));
+                //Response.End();
+
+            }
+            catch (Exception ex)
+            {
+                lblError.ForeColor = System.Drawing.Color.Red;
+                lblError.Text = "Error parsing html!";
+            }
+        }
+
+
+        string GenerateICalEvent(DateTime start, DateTime end, List<string> lesson)
+        {
+            string iEvent = "";
+
+            if (lesson[5].Equals("&nbsp;"))
+                lesson[5] = "";
+
+            iEvent += "BEGIN:VEVENT" + "\r\n";
+            iEvent += "DTSTART;TZID=\"Singapore Standard Time\":" + ConvertToDateString(start) + "\r\n";
+            iEvent += "DTEND;TZID=\"Singapore Standard Time\":" + ConvertToDateString(end) + "\r\n";
+            iEvent += "LOCATION:" + lesson[13] + " (" + lesson[9] + ")" + "\r\n";
+            iEvent += "SUMMARY:" + lesson[0] + "-" + lesson[1] + "\r\n";
+            iEvent += "DESCRIPTION:" + "Map: http://maps.ntu.edu.sg/m?q=" + lesson[13] + "\\nGroup: " + lesson[10] + "\\n" + "Index: " + lesson[6] + "\\n" + "Course Type: " + lesson[3] + "\\n" + "AU: " + lesson[2] + "\\n" + lesson[5] + "\\n" + "\r\n";
+            iEvent += "END:VEVENT" + "\r\n";
+
+            return iEvent;
+        }
+
+        byte[] GetBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+        string ConvertToDateString(DateTime dt)
+        {
+            return dt.ToString("yyyyMMddTHHmmss");
+        }
+
+        Boolean CheckBlank(List<List<string>> data, int i, int j)
+        {
+            if (data[i][j].Equals("&nbsp;") || data[i][j].Equals(""))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        string Abbr(string subject)
+        {
+            string[] words = subject.Split(' ');
+            subject = "";
+            foreach (string word in words)
+            {
+                if (word.Length > 4)
+                {
+                    subject += word.Substring(0, 4) + " ";
+                }
+                else
+                {
+                    subject += word + " ";
+                }
+
+            }
+            return subject;
+        }
+    }
+}
